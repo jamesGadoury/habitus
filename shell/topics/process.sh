7f94115fc706454fc1a624e83f22d6cda7f8c856
James Gadoury <gadouryjames@gmail.com>
2026-05-16 13:57:57 -0400

    shell: expand psids matches to full pgid/sid context
    Two-pass awk: collect the pgid/sid of any row matching the
    pattern, then emit every row sharing one of those groups.
    Hunting a process usually means wanting its parents and
    pipeline siblings, not just the literal match.

--- shell/topics/process.sh
+++ shell/topics/process.sh
@@ -4,6 +4,18 @@ psids() {
   if [ $# -eq 0 ]; then
     ps -eo pid,pgid,sid,comm
   else
-    ps -eo pid,pgid,sid,comm | awk -v pat="$*" 'NR==1 || $0 ~ pat'
+    ps -eo pid,pgid,sid,comm | awk -v pat="$*" '
+      NR == 1 { print; next }
+      {
+        lines[NR] = $0
+        pgid_of[NR] = $2
+        sid_of[NR] = $3
+        if ($0 ~ pat) { keep_pgid[$2] = 1; keep_sid[$3] = 1 }
+      }
+      END {
+        for (i = 2; i <= NR; i++)
+          if (keep_pgid[pgid_of[i]] || keep_sid[sid_of[i]]) print lines[i]
+      }
+    '
   fi
 }
