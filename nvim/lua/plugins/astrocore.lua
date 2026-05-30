e6b8df682b3a81bf798bb34d74c3d804621fce5d
James Gadoury <gadouryjames@gmail.com>
2026-05-30 16:01:28 -0400

    nvim: default to 4-space indent

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -45,6 +43,10 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        expandtab = true, -- use spaces instead of tab characters
+        tabstop = 4, -- width of a tab character
+        shiftwidth = 4, -- spaces per indent step
+        softtabstop = 4, -- spaces inserted by <Tab> in insert mode
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
