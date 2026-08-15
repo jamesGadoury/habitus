0589154720f5f1fec584bab8e5cc1e9cf3f198fc
James Gadoury <gadouryjames@gmail.com>
2026-08-15 14:00:24 -0400

    nvim: add mermaid treesitter parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,8 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      -- highlights ```mermaid fences; markdown-preview.nvim renders them
+      "mermaid",
       -- add more arguments for adding more treesitter parsers
     },
   },
